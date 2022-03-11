9bdf04b2912f7d44af29abcacdaa9d5cb107ee33
LinnykerEros <54625876+LinnykerEros@users.noreply.github.com>
2022-03-11 15:32:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ let capaFilmes = [
   "https://i.pinimg.com/originals/50/8f/ba/508fba93c5f1b8f1ea1db81dd969e2a0.jpg",
   "https://www.planocritico.com/wp-content/uploads/2021/01/175-Lista-Os-Melhores-Episodios-de-Vikings-1.jpg",
   "https://i.pinimg.com/550x/56/a0/be/56a0bec9ff88fbb573f9bf176e487e0f.jpg",
+  "https://i.ytimg.com/vi/aO-ZaF4FJls/maxresdefault.jpg",
 ];
 
 let videos = [
-  "https://www.youtube.com/embed/Ro0yIrOKgbM",
+   "https://www.youtube.com/embed/Ro0yIrOKgbM",
   "https://www.youtube.com/embed/dV6p83HRk4o",
   "https://www.youtube.com/embed/iSiqmV13Ffk",
+  "https://www.youtube.com/embed/aO-ZaF4FJls",
 ];
 
 //COLOCO A CAPA NA TELA PASSANDO A FUNÇÃO DO FRAME NO CLICK
